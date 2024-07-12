c432599ea901aa7e873b9d1200b1c95f61cdf108
Atharva Inamdar <123922466+Atharvai2809@users.noreply.github.com>
2024-07-12 11:55:06 +0530

    Update TodoItem.js

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { List, Button } from 'antd';
+import { List, Button, Checkbox } from 'antd';
 
-const TodoItem = ({ item, index, showModal, deleteTask }) => {
+const TodoItem = ({ item, index, showModal, deleteTask, toggleCompleteTask }) => {
   return (
     <List.Item
       actions={[
+        <Checkbox checked={item.completed} onChange={() => toggleCompleteTask(index)}>
+          Completed
+        </Checkbox>,
         <Button type="link" onClick={() => showModal(index)}>
           Edit
         </Button>,
@@ -12,8 +15,11 @@ const TodoItem = ({ item, index, showModal, deleteTask }) => {
           Delete
         </Button>,
       ]}
+      style={{ display: 'flex', justifyContent: 'space-between' }}
     >
-      {item.text}
+      <span style={{ textDecoration: item.completed ? 'line-through' : 'none', flex: 1 }}>
+        {item.text}
+      </span>
     </List.Item>
   );
 };
